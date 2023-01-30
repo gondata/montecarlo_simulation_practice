e05e9651da7bdb3c248c0fd4bd3801a2209990d5
Gonzalo Noblega <gnnoblega@gmail.com>
2023-01-30 14:27:49 -0300

    Fix some things

--- montecarlo_simulation.py
+++ montecarlo_simulation.py
@@ -6,7 +6,7 @@ from pandas_datareader import data as pdr
 import yfinance as yf
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib import style #Allows us to choose the chart's color
+from matplotlib import style # Allows us to choose the chart's color
 from scipy.stats import norm
 
 yf.pdr_override()
@@ -26,8 +26,8 @@ data[ticker] = pdr.get_data_yahoo(ticker, start=startdate, end=enddate)['Adj Clo
 log_returns = np.log(1+data.pct_change())
 u = log_returns.mean()
 var = log_returns.var()
-drift = u - (0.5*var) #Growth or decrease
-std = log_returns.std() #Standard deviation due to we are working with a population and not with a sample
+drift = u - (0.5*var) # Growth or decrease
+std = log_returns.std() # Standard deviation due to we are working with a population and not with a sample
 
 # Variables
 
@@ -36,10 +36,10 @@ trials = 10000
 
 # Now we need to add to our model the random component
 
-Z = norm.ppf(np.random.rand(days,trials))
-daily_returns = np.exp(drift.values + std.values * Z)
-price_path = np.zeros_like(daily_returns)
-price_path[0] = data.iloc[-1]
+Z = norm.ppf(np.random.rand(days,trials)) # We generate random values for a defined array
+daily_returns = np.exp(drift.values + std.values * Z) # We generate the daily returns for each simulation based on a normal distribution
+price_path = np.zeros_like(daily_returns) # We build the matrix that will allow us to make the multiplications
+price_path[0] = data.iloc[-1] # We fix the day for the calculus
 
 for t in range(1, days):
     price_path[t] = price_path[t-1]*daily_returns[t]
